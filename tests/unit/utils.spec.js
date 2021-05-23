1d788e1dda0715602382c6770d993fc14876421a
SantiagoAlbea <santiialbea123@gmail.com>
2021-05-23 13:31:46 -0300

    Test de montos con decimales

--- tests/unit/utils.spec.js
+++ tests/unit/utils.spec.js
@@ -12,3 +12,14 @@ test('Should convert undefined into ""', async () => {
     expect(monefy(undefined)).toBe('');
 });
 
+test('Should convert 200.5 into "200,50"', async () => {
+    expect(monefy(200.5)).toBe('200,50');
+});
+
+test('Should convert 2000.5 into "2.000,50"', async () => {
+    expect(monefy(2000.5)).toBe('2.000,50');
+});
+
+test('Should convert 2000.555 into "2.000,55"', async () => {
+    expect(monefy(2000.555)).toBe('2.000,55');
+});
