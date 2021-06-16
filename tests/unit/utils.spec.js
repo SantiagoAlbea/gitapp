626d86fe7f532ee74fa36b0bd79255ea728a313c
SantiagoAlbea <santiialbea123@gmail.com>
2021-06-16 11:53:52 -0300

    Agrega test unitario para la funcion de la fecha

--- tests/unit/utils.spec.js
+++ tests/unit/utils.spec.js
@@ -1,4 +1,4 @@
-const { monefy } = require('../../client/js/utils.js');
+const { monefy, friendlyDate } = require('../../client/js/utils.js');
 
 test('Should convert 1000 into "1.000"', async () => {
     expect(monefy(1000)).toBe('1.000');
@@ -22,4 +22,24 @@ test('Should convert 2000.5 into "2.000,50"', async () => {
 
 test('Should convert 2000.555 into "2.000,55"', async () => {
     expect(monefy(2000.555)).toBe('2.000,55');
+});
+
+test('Should convert "2021-06-16T14:31:54.214Z" into "16/6/2021"', async () => {
+    expect(friendlyDate('2021-06-16T14:31:54.214Z')).toBe('16/6/2021');
+});
+
+test('Should convert "2021-01-01T03:00:00.000Z" into "1/1/2021"', async () => {
+    expect(friendlyDate('2021-01-01T03:00:00.000Z')).toBe('1/1/2021');
+});
+
+test('Should convert "Wed Jun 16 2021" into "16/6/2021"', async () => {
+    expect(friendlyDate('Wed Jun 16 2021')).toBe('16/6/2021');
+});
+
+test('Should convert "Wed Jun 16 2021 11:42:46 GMT-0300" into "16/6/2021"', async () => {
+    expect(friendlyDate('Wed Jun 16 2021 11:42:46 GMT-0300')).toBe('16/6/2021');
+});
+
+test('Should convert "Wed, 16 Jun 2021 14:42:46 GMT" into "16/6/2021"', async () => {
+    expect(friendlyDate('Wed, 16 Jun 2021 14:42:46 GMT')).toBe('16/6/2021');
 });
